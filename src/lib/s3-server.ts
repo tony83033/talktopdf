d306a70c6ef11a96d74d311f41764966d5609706
Sumit pal <tonystark83033@gmail.com>
2023-11-04 09:03:03 -0700

    fix: [fix read file issue hackcbs by mlh]

--- src/lib/s3-server.ts
+++ src/lib/s3-server.ts
@@ -1,37 +1,42 @@
 import AWS from 'aws-sdk';
 import fs from 'fs';
-export async function downloadFromS3(file_key:string){
-    try {
-        AWS.config.update({
-            accessKeyId: process.env.NEXT_PUBLIC_S3_AWS_ACCESS_KEY_ID,
-            secretAccessKey: process.env.NEXT_PUBLIC_S3_AWS_SECRET_ACCESS_KEY,
-        });
+import path from 'path';
 
-        const s3 = new AWS.S3({
-            params:{
-                Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
-            },
-            region: "ap-south-1"
-        });
+export async function downloadFromS3(file_key: string) {
+  try {
+    AWS.config.update({
+      accessKeyId: process.env.NEXT_PUBLIC_S3_AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.NEXT_PUBLIC_S3_AWS_SECRET_ACCESS_KEY,
+    });
 
-        const params = {
-            Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
-            Key: file_key,
-        };
+    const s3 = new AWS.S3({
+      params: {
+        Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
+      },
+      region: "ap-south-1",
+    });
 
-        const obj = await s3.getObject(params).promise(); // get pdf from aws s3
-        
-        const file_name = `/tmp/pdf-${Date.now()}.pdf`; // create a file 
-        fs.writeFileSync(file_name,obj.Body as Buffer); // store pdf content in file_name
-        
-        return file_name;
+    const params = {
+      Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+      Key: file_key,
+    };
 
+    const obj = await s3.getObject(params).promise();
 
+    const directoryPath = './tmp/'; // Change this to the desired directory path
 
-        
-    } catch (error) {
-        console.log(error);
-        return null;
-        
+    // Ensure that the directory exists, or create it if it doesn't
+    if (!fs.existsSync(directoryPath)) {
+      fs.mkdirSync(directoryPath, { recursive: true });
     }
+
+    const file_name = path.join(directoryPath, `pdf-${Date.now()}.pdf`);
+
+    await fs.writeFileSync(file_name, obj.Body as Buffer);
+
+    return file_name;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 }
